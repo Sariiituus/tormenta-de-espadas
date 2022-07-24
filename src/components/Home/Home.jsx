d09c0c45d033d7483579b27e449a4aaeab25b150
reyesgarcia-ana <reyesgarzia.ana@gmail.com>
2022-07-24 19:13:30 +0200

    home finished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,12 +8,15 @@ const Home = (props) => {
     <div className='text-wrapper'>
       
       <h1 className='titulo'>{ datos.titulo }</h1>
+
+      <img src='./assets/icons/logo-white-bg.svg' alt="" />
       
       <div className='info-wrapper'>
         <p className='parrafoUno'>{ datos.parrafoUno }</p>
       </div>
 
       <button class="button-6" role="button">¡Comienza tu aventura!</button>
+
       
     </div>
     
