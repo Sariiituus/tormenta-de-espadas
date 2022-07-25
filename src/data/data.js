7e134535506f3e0ac881c812e4c70a62ea569f64
reyesgarcia-ana <reyesgarzia.ana@gmail.com>
2022-07-25 16:38:37 +0200

    estilos1638

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 export const allData = {
   home: {
-    titulo: "Bienvenido al campamento Tormeta de Espadas",
+    titulo: "Bienvenido al campamento Tormenta de Espadas",
     parrafoUno:"¡Súbditos de los Siete Reinos! Se hace saber que Robert Baratheon, rey de los Ándalos, Primero de su Nombre, ordena la presencia de las casas Stark, Lannister, Martell y Greyjoy en Desembarco del Rey para celebrar su 35º cumpleaños. Las familias se enfrentarán en competición honorable durante el último fin de semana del séptimo mes del año para determinar de cuál de ellas saldrá la próxima Mano del Rey. ¡Larga vida al rey Robert!",
     
   },
