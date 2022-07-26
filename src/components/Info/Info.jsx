e3b0c3dd9597ab710a97a93b9b44b0dc06eb3e15
reyesgarcia-ana <reyesgarzia.ana@gmail.com>
2022-07-26 20:14:14 +0200

    estilos info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -6,44 +6,52 @@ const Info = (props) => {
   const { datos } = props;
 
   return (
-    <div className='info-container'>
-      <div className='info-titulo-subtitulo-container'>
-        <div className='info-titulo'>{ datos.titulo }</div>
-        <div className='info-subtitulo'>{ datos.subtitulo }</div>
-      </div>
+    
+    <div className='content-wrapper'>
 
-      <div className='info-contenido-container'>
-        <div className='info-descripcion-container'>
-          <p>bla bla bla bla</p>
-          <p>ble ble ble</p>
-          <p>blo blo bl</p>
-          <p>blu blu blu</p>
+      <div className='title'>
+          { datos.titulo }
         </div>
 
-        <div className='info-mapa-container'>
-          <div>
-            <iframe
-              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2942.462090676946!2d-1.651578584815165!3d42.48172783540235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5a771683921c8d%3A0x25ce4904b49ea416!2sPalacio%20Real%20de%20Olite!5e0!3m2!1ses!2ses!4v1658587558341!5m2!1ses!2ses'
-              width='400'
-              height='300'
-              allowfullscreen=''
-              loading='lazy'
-              title='Mapa'
-              referrerpolicy='no-referrer-when-downgrade'
-            ></iframe>
-          </div>
-        </div>
-      </div>
+      <div className="info-wrapper">
 
-      <div className='info-calendario-container'>
-        <div>
-            <h4>Calendario</h4>
+        <div className='description'>
+          <p>{ datos.parrafoUno }</p>
+          <p>{ datos.parrafoDos }</p>
+          <p>{ datos.parrafoTres }</p>
+          <p>{ datos.parrafoCuatro }</p>
+        </div>
+
+        <div className='map'>
+
+          <iframe
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2942.462090676946!2d-1.651578584815165!3d42.48172783540235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5a771683921c8d%3A0x25ce4904b49ea416!2sPalacio%20Real%20de%20Olite!5e0!3m2!1ses!2ses!4v1658587558341!5m2!1ses!2ses'
+            width='400'
+            height='300'
+            allowfullscreen=''
+            loading='lazy'
+            title='Mapa'
+            referrerpolicy='no-referrer-when-downgrade'
+          ></iframe>
+        
+        </div>
+
+      </div>
+      
+      <div className='calendar-wrapper'>
+
+        <div className="calendar-title">
+            <h4>Calendario de actividades</h4>
         </div>
         
-        <div>
-            <button>Descargar</button>
+        <div className="calendar-btn">
+        <a className="calendario" download href='/assets/Actividades-TDE.pdf' role="button">
+          Descargar
+          </a>
         </div>
+
       </div>
+
     </div>
   );
 };
