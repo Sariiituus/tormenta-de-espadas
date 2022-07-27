c63d34bf77f527f30ace87bafe0b87f5cf02e5f8
Sariiituus <sara.rodriguez@bootcamp-upgrade.com>
2022-07-27 12:56:25 +0200

    footer retocado

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import NavBar from "./components/NavBar/NavBar";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import { getRoutes } from "./data/routes";
 import { allData } from "./data/data";
-import galleryData from "./components/Gallery/GalleryData.js"
+import galleryData from "./components/Gallery/GalleryData.js";
 import CharacterProfile from "./components/CharacterProfile/CharacterProfile";
-
 
 const routes = getRoutes(
   allData.home,
@@ -15,32 +14,44 @@ const routes = getRoutes(
 );
 
 const App = () => {
-
   let isLoggedIn = false; // TODO: Sacarlo del estado o sacarlo de redux;
 
-  const routesToAdd = isLoggedIn ? routes.routesPrivateWebsite : routes.routesPublicWebsite;
+  const routesToAdd = isLoggedIn
+    ? routes.routesPrivateWebsite
+    : routes.routesPublicWebsite;
 
   return (
     <div className='app'>
       <Router>
-        
         <header className='app-header'>
           <NavBar routes={routes} isLoggedIn={isLoggedIn}></NavBar>
         </header>
-        
+
         <main className='app-content'>
           <Routes>
             {routesToAdd.map((route) => {
-              return <Route path={route.path} element={route.element} key={route.path} />;
+              return (
+                <Route
+                  path={route.path}
+                  element={route.element}
+                  key={route.path}
+                />
+              );
             })}
           </Routes>
         </main>
-        
-        <footer className='app-footer'>Pie de pagina</footer>
 
+        <footer className='app-footer'>
+          <p className='copyright'>© </p>
+          <p className='footer-link first-link'>Cookies</p>
+          <p className='footer-link'>Aviso legal</p>
+          <p className='footer-link'>Política de privacidad</p>
+          <p className='footer-link'>Condiciones de uso en RR.SS</p>
+          <p className='footer-link'>Política de Privacidad en RR.SS</p>
+        </footer>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
